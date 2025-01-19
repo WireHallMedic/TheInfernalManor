d6c903ea13801b2b22e5d8dd37154a96d46f2ca3
Michael <MichaelWidler@yahoo.com>
2025-01-19 10:22:45 -0800

    Basic application setup.

--- GUI/TIMFrame.java
+++ GUI/TIMFrame.java
@@ -1,6 +1,26 @@
 package TheInfernalManor.GUI;
 
-public class TIMFrame
-{
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import StrictCurses.*;
 
+public class TIMFrame extends JFrame implements SCConstants
+{
+   private SCTilePalette palette;
+   private Vector<SCPanel> panelList;
+   
+   public TIMFrame()
+   {
+      super();      
+      setSize(1500, 800);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setTitle("The Infernal Manor");
+      setLayout(null);
+      setBackground(new Color(DEFAULT_BG_COLOR));
+      
+      panelList = new Vector<SCPanel>();
+      
+      setVisible(true);
+   }
 }
--- TIMMain.java
+++ TIMMain.java
@@ -1,8 +1,12 @@
 package TheInfernalManor;
 
+import TheInfernalManor.GUI.*;
 
 
 public class TIMMain
 {
-
+   public static void main(String[] args)
+   {
+      TIMFrame frame = new TIMFrame();
+   }
 }
