703edec3eab032e86ef0ac1485a9d5139b376dcd
Michael <MichaelWidler@yahoo.com>
2025-01-20 10:25:33 -0800

    Add duplicates to make the rest of the panels.

---
+++ GUI/InventoryPanel.java
@@ -0,0 +1,19 @@
+package TheInfernalManor.GUI;
+
+import StrictCurses.*;
+import java.awt.event.*;
+
+public class HelpPanel extends TIMPanel implements GUIConstants
+{
+   public HelpPanel(SCTilePalette tilePalette, TIMFrame pFrame)
+   {
+      super(tilePalette, pFrame);
+      GUITools.drawBorderWithTitle(this, " Help ");
+   }
+   
+   @Override
+   public void keyPressed(KeyEvent ke)
+   {
+      parentFrame.returnToLastPanel();
+   }
+}
