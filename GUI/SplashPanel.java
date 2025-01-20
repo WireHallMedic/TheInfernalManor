ff4fcaac3aed068f08de4fa24991e488ec4d2edc
Michael <MichaelWidler@yahoo.com>
2025-01-20 08:28:15 -0800

    Add options to splash panel.

--- GUI/SplashPanel.java
+++ GUI/SplashPanel.java
@@ -5,10 +5,14 @@ import java.awt.event.*;
 
 public class SplashPanel extends TIMPanel implements GUIConstants
 {
+   private String[] optionList = {"[N]ew Game", "[L]oad Game", "[P]references", "[H]elp", "[Q]uit"};
+   
    public SplashPanel(SCTilePalette tilePalette, TIMFrame pFrame)
    {
       super(tilePalette, pFrame);
-      writeLine(0, 0, "SplashPanel");
+      writeLine(0, 0, "The Infernal Manor");
+      for(int i = 0; i < optionList.length; i++)
+         writeLine(5, 20 + i, optionList[i]);
    }
    
    @Override
