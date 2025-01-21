c1f9657fb46c6f6e2c6d4e2050fe7b8c4cae26ad
Michael <MichaelWidler@yahoo.com>
2025-01-21 05:27:49 -0800

    Implement zonemap.

---
+++ Map/ZoneMap.java
@@ -0,0 +1,61 @@
+package TheInfernalManor.Map;
+
+public class ZoneMap
+{
+	private int width;
+	private int height;
+	private String name;
+	private boolean[][] lowPassMap;
+	private boolean[][] highPassMap;
+	private boolean[][] transparentMap;
+	private MapCell[][] tileMap;
+
+
+	public int getWidth(){return width;}
+	public int getHeight(){return height;}
+	public String getName(){return name;}
+	public boolean[][] getLowPassMap(){return lowPassMap;}
+	public boolean[][] getHighPassMap(){return highPassMap;}
+	public boolean[][] getTransparentMap(){return transparentMap;}
+	public MapCell[][] getTileMap(){return tileMap;}
+
+
+	public void setName(String n){name = n;}
+
+
+   public ZoneMap(int w, int h)
+   {
+      setSize(w, h);
+   }
+   
+   public void setSize(int w, int h)
+   {
+      width = w;
+      height = h;
+      lowPassMap = new boolean[w][h];
+      highPassMap = new boolean[w][h];
+      transparentMap = new boolean[w][h];
+      tileMap = new MapCell[w][h];
+   }
+   
+   public boolean isInBounds(int x, int y)
+   {
+      return x >= 0 && x < width &&
+         y >= 0 && y < height;
+   }
+   
+   public boolean isLowPassable(int x, int y)
+   {
+      return isInBounds(x, y) && lowPassMap[x][y];
+   }
+   
+   public boolean isHighPassable(int x, int y)
+   {
+      return isInBounds(x, y) && highPassMap[x][y];
+   }
+   
+   public boolean isTransparent(int x, int y)
+   {
+      return isInBounds(x, y) && transparentMap[x][y];
+   }
+}
