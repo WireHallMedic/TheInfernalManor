e733b41415e2f1e29375189ca3c04fb31c63fea4
Michael <MichaelWidler@yahoo.com>
2025-01-21 05:11:25 -0800

    Fill out base tile types.

--- Map/MapCellBase.java
+++ Map/MapCellBase.java
@@ -4,11 +4,19 @@ import StrictCurses.*;
 
 public enum MapCellBase
 {
-   CLEAR          (SCConstants.SMALL_BULLET_TILE, true, true, true),
-   WALL           ('#', false, false, false),
-   LOW_WALL       ('=', false, true, true),
-   SHALLOW_LIQUID ('~', false, true, true),
-   DEEP_LIQUID    (SCConstants.ALMOST_EQUAL_TO_TILE, false, true, true);
+   CLEAR             (SCConstants.BULLET_TILE, true, true, true),
+   ROUGH             (SCConstants.CENTERED_COMMA_TILE, true, true, true),
+   WALL              ('#', false, false, false),
+   LOW_WALL          ('=', false, true, true),
+   BARS              (':', false, false, true),
+   SHALLOW_LIQUID    ('~', false, true, true),
+   DEEP_LIQUID       (SCConstants.ALMOST_EQUAL_TO_TILE, false, true, true),
+   CHEST_CLOSED      ('?', false, true, true),
+   CHEST_OPEN        (SCConstants.INVERTED_QUESTION_TILE, false, true, true),
+   DOOR_CLOSED       ('|', false, false, false),
+   DOOR_OPEN         ('-', true, true, true),
+   TOGGLE_UNFLIPPED  ('!', false, false, true),
+   TOGGLE_FLIPPED    (SCConstants.INVERTED_EXCLAMATION_TILE, false, false, true);;
    
    public int iconIndex;
    public boolean lowPassable;
