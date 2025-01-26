c48abb732bba3dfbcce7411a39742fa5b633c0cb
Michael <MichaelWidler@yahoo.com>
2025-01-26 13:30:10 -0800

    Add relic slot restrictions.

--- TheInfernalManor/Item/Relic.java
+++ TheInfernalManor/Item/Relic.java
@@ -4,6 +4,21 @@ import TheInfernalManor.GUI.*;
 
 public class Relic extends Item implements GUIConstants
 {
+   public enum Restriction
+   {
+      HEAD  ("Head"),
+      HANDS ("Hands"),
+      FEET  ("Feet"),
+      ARMS  ("Arms");
+      
+      public String string;
+      
+      private Restriction(String s)
+      {
+         string = s;
+      }
+   }
+   
    public Relic(String n)
    {
       super(n, RELIC_ICON, WHITE);
