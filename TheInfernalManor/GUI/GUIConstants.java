d333e6f0ecf00cfb7c686467eb6dfa59b804ea83
Michael <MichaelWidler@yahoo.com>
2025-01-25 19:13:34 -0800

    Tweak colors.

--- TheInfernalManor/GUI/GUIConstants.java
+++ TheInfernalManor/GUI/GUIConstants.java
@@ -22,18 +22,18 @@ public interface GUIConstants
    public static final int WHITE = Color.WHITE.getRGB();
    public static final int BLACK = Color.BLACK.getRGB();
    public static final int RED = -39322;
-   public static final int DARK_RED = -4174515;
+   public static final int DARK_RED = -7981771;
    public static final int ORANGE = -8602;
    public static final int YELLOW = -154;
    public static final int GREEN = -11681715;
-   public static final int DARK_GREEN = -13402061;
+   public static final int DARK_GREEN = -14460637;
    public static final int BLUE = -10066177;
    public static final int DARK_BLUE = -13421696;
    public static final int PURPLE = -8375424;
    public static final int PINK = -20561;
    public static final int DARK_PINK = -2583404;
-   public static final int BROWN = -12111588;
-   public static final int DARK_BROWN = -10074327;
+   public static final int BROWN = -10074327;
+   public static final int DARK_BROWN = -14543091;
    public static final int LIGHT_GREY = -4144960;
    public static final int GREY = -8355712;
    public static final int DARK_GREY = -12566464;
@@ -47,4 +47,5 @@ public interface GUIConstants
    public static final int[] DARK_BLUE_GRADIENT = GUITools.getGradient(DARK_BLUE);
    public static final int[] DARK_BROWN_GRADIENT = GUITools.getGradient(DARK_BROWN);
    public static final int[] DARK_GREY_GRADIENT = GUITools.getGradient(DARK_GREY);
+   public static final int[] DARK_RED_GRADIENT = GUITools.getGradient(DARK_RED);
 }
