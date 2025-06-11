f45ce5f88348d5c57a41df885fcde058290ac0a1
Michael <MichaelWidler@yahoo.com>
2025-06-11 16:35:22 -0700

    Build structure for RoomTemplateDeck.

--- TheInfernalManor/Map/RoomTemplateDeck.java
+++ TheInfernalManor/Map/RoomTemplateDeck.java
@@ -2,7 +2,29 @@ package TheInfernalManor.Map;
 
 import java.util.*;
 
-public class RoomTemplateDeck
+public class RoomTemplateDeck implements MapConstants
 {
-   private Vector<RoomTemplate>[] type;
+   private RTCollection[] typeList;
+   
+   public RoomTemplateDeck()
+   {
+      typeList = new RTCollection[ConnectionType.values().length];
+      for(int i = 0; i < ConnectionType.values().length; i++)
+      {
+         typeList[i] = new RTCollection(ConnectionType.values()[i]);
+      }
+   }
+   
+   
+   private class RTCollection
+   {
+      public ConnectionType type;
+      public Vector<RoomTemplate> list;
+      
+      public RTCollection(ConnectionType t)
+      {
+         type = t;
+         list = new Vector<RoomTemplate>();
+      }
+   }
 }
