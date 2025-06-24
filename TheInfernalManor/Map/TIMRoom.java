1affbea119f4bcb9714f47cc5fae9b67eb40549c
Michael <MichaelWidler@yahoo.com>
2025-06-23 21:24:26 -0700

    Override WS random functions.

--- TheInfernalManor/Map/TIMRoom.java
+++ TheInfernalManor/Map/TIMRoom.java
@@ -6,6 +6,7 @@ Adds a bit more functionality to WidlerSuite.Room
 package TheInfernalManor.Map;
 
 import WidlerSuite.*;
+import TheInfernalManor.Engine.*;
 
 public class TIMRoom extends Room
 {
@@ -78,5 +79,49 @@ public class TIMRoom extends Room
    private boolean intervalOverlap(int max1, int max2, int min1, int min2)
    {
       return (max1 >= min2) & (max2 >= min1);
+   }
+   
+   // returns true if the right boundary of this room and the left boundary of that room are the same.
+   // find sibiling then checks the opposite (right of that adjacent to left of this)
+   @Override
+   public boolean isHorizontallyAdjacent(Room that, boolean findSibiling)
+   {
+      if(this.origin.x + this.size.x - 1 == that.origin.x)
+         return true;
+      
+      if(findSibiling == false)
+      if(that.origin.x + that.size.x - 1 == this.origin.x)
+         return true;
+      
+      return false;
+   }
+   public boolean isHorizontallyAdjacent(Room that){return isHorizontallyAdjacent(that, false);}
+   
+   // returns true if the bottom boundary of this room and the top boundary of that room are the same.
+   // find sibiling then checks the opposite (bottom of that adjacent to top of this)
+   @Override
+   public boolean isVerticallyAdjacent(Room that, boolean findSibiling)
+   {
+      if(this.origin.y + this.size.y - 1 == that.origin.y)
+         return true;
+      
+      if(findSibiling == false)
+      if(that.origin.y + that.size.y - 1 == this.origin.y)
+         return true;
+      
+      return false;
+   }
+   public boolean isVerticallyAdjacent(Room that){return isVerticallyAdjacent(that, false);}
+   
+   // returns a random cell from the room
+   @Override
+   public Coord getRandomCell()
+   {
+      Coord cell = new Coord(origin);
+      
+      cell.x += RNG.nextInt(size.x);
+      cell.y += RNG.nextInt(size.y);
+      
+      return cell;
    }
 }
