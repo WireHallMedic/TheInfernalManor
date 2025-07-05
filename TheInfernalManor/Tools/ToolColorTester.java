74896c05ff295b8abec67947a305dafb2efc9ac1
Michael <MichaelWidler@yahoo.com>
2025-07-05 13:10:10 -0700

    Add tool for testing FG/BG color combinations.

---
+++ TheInfernalManor/Tools/ToolColorTester.java
@@ -0,0 +1,14 @@
+package TheInfernalManor.Tools;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import TheInfernalManor.GUI.*;
+import StrictCurses.*;
+
+
+public class ToolColorTester extends JFrame implements GUIConstants, MouseListener
+{
+
+}
