ad539eae64408fc1694180cac9ac1c00219a3446
Michael <MichaelWidler@yahoo.com>
2025-07-05 13:50:28 -0700

    Finish colorTester.

--- TheInfernalManor/Tools/ToolColorTester.java
+++ TheInfernalManor/Tools/ToolColorTester.java
@@ -22,7 +22,7 @@ public class ToolColorTester extends JFrame implements GUIConstants, MouseListen
       setTitle("Color Tester");
       
       setLayout(new GridLayout(1, 1));
-      tilePanel = new SCPanel(new SCTilePalette("WidlerTiles_16x16.png", 16, 16), 10, 10);
+      tilePanel = new SCPanel(new SCTilePalette("WidlerTiles_16x16.png", 16, 16), 10, 5);
       add(tilePanel);
       for(int i = 0; i < BG_COLOR_ARRAY.length; i++)
       {
@@ -36,7 +36,26 @@ public class ToolColorTester extends JFrame implements GUIConstants, MouseListen
          tilePanel.setTileBG(x, y, COLOR_ARRAY[i]);
       }
       
+      tilePanel.addMouseListener(this);
       setVisible(true);
+   }
+   
+   public boolean isValidColor(int x, int y)
+   {
+      int i = x % 10;
+      i += 10 * (y - 2);
+      return i >= 0 && i < COLOR_ARRAY.length;
+   }
+   
+   public int getFGColor(int x, int y)
+   {
+      if(isValidColor(x, y))
+      {
+         int i = x % 10;
+         i += 10 * (y - 2);
+         return COLOR_ARRAY[i];
+      }
+      return 0;
    }
    
    public void mousePressed(MouseEvent me){}
@@ -44,7 +63,17 @@ public class ToolColorTester extends JFrame implements GUIConstants, MouseListen
    public void mouseEntered(MouseEvent me){}
    public void mouseExited(MouseEvent me){}
    
-   public void mouseClicked(MouseEvent me){}
+   public void mouseClicked(MouseEvent me)
+   {
+      int[] loc = tilePanel.getMouseLocTile();
+      if(isValidColor(loc[0], loc[1]))
+      {
+         int fgColor = getFGColor(loc[0], loc[1]);
+         for(int x = 0; x < BG_COLOR_ARRAY.length; x++)
+            tilePanel.setTileFG(x, 0, fgColor);
+      }
+      tilePanel.repaint();
+   }
    
    public static void main(String[] args)
    {
