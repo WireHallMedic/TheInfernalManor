ff3ed62334070b0efa6b351d2d34b395f6849a33
Michael <MichaelWidler@yahoo.com>
2025-07-20 04:31:40 -0700

    Create file for item affixes.

--- TheInfernalManor/Item/ItemConstants.java
+++ TheInfernalManor/Item/ItemConstants.java
@@ -8,26 +8,29 @@ public interface ItemConstants
 {  
    public enum RelicBase implements Rollable
    {
-      HELM     (0, 30, 5),
-      GLOVES   (0, 30, 5),
-      BOOTS    (0, 30, 5),
-      BRACERS  (0, 30, 5),
-      AMULET   (0, 30, 5);
+      HELM     (0, 30, 5, Relic.Restriction.HEAD),
+      GLOVES   (0, 30, 5, Relic.Restriction.HANDS),
+      BOOTS    (0, 30, 5, Relic.Restriction.FEET),
+      BRACERS  (0, 30, 5, Relic.Restriction.ARMS),
+      JEWELRY  (0, 30, 5, null);
       
       private int minLevel;
       private int maxLevel;
       private int weight;
+      private Relic.Restriction restriction;
       
-      private RelicBase(int min, int max, int w)
+      private RelicBase(int min, int max, int w, Relic.Restriction r)
       {
          minLevel = min;
          maxLevel = max;
          weight = w;
+         restriction = r;
       }
       
       public int getMinLevel(){return minLevel;}
       public int getMaxLevel(){return maxLevel;}
       public int getWeight(){return weight;}
+      public Relic.Restriction getRestriction(){return restriction;}
    }
    
    
--- TheInfernalManor/Item/RelicFactory.java
+++ TheInfernalManor/Item/RelicFactory.java
@@ -5,59 +5,26 @@ import TheInfernalManor.Engine.*;
 
 public class RelicFactory implements GUIConstants, ItemConstants
 {
+   private static final String[] JEWELRY_NAMES = {"Ring", "Amulet", "Bracelet", "Gem", "Charm"};
    public static Relic randomRelic(int level)
    {
       RelicBase roll = (RelicBase)EngineTools.roll(RelicBase.values(), level);
       Relic r = new Relic("temp");
+      r.setRestriction(roll.getRestriction());
       switch(roll)
       {
-         case HELM:        r = getHelm();
+         case HELM:        r.setName("Helm");
                            break;
-         case GLOVES:      r = getGloves();
+         case GLOVES:      r.setName("Gloves");
                            break; 
-         case BOOTS:       r = getBoots();
+         case BOOTS:       r.setName("Boots");
                            break;
-         case BRACERS:     r = getBracers();
+         case BRACERS:     r.setName("Bracers");
                            break;
-         case AMULET:      r = getAmulet();
+         case JEWELRY:     r.setName(JEWELRY_NAMES[RNG.nextInt(JEWELRY_NAMES.length)]);
                            break;
       }
       return r;
    }
    
-   public static Relic getHelm()
-   {
-      Relic r = new Relic("Helm");
-      r.setRestriction(Relic.Restriction.HEAD);
-      return r;
-   }
-
-   public static Relic getGloves()
-   {
-      Relic r = new Relic("Gloves");
-      r.setRestriction(Relic.Restriction.HANDS);
-      return r;
-   }
-
-   public static Relic getBoots()
-   {
-      Relic r = new Relic("Boots");
-      r.setRestriction(Relic.Restriction.FEET);
-      return r;
-   }
-
-   public static Relic getBracers()
-   {
-      Relic r = new Relic("Bracers");
-      r.setRestriction(Relic.Restriction.ARMS);
-      return r;
-   }
-   
-   public static Relic getAmulet()
-   {
-      Relic r = new Relic("Amulet");
-      return r;
-   }
-
-
 }
