4bc99d81fec9afe02e2cb50a49cacfabff85b33e
Michael <MichaelWidler@yahoo.com>
2025-07-20 04:34:21 -0700

    Create EquipmentAffexFactory.

---
+++ TheInfernalManor/Item/EquipmentAffexFactory.java
@@ -0,0 +1,11 @@
+package TheInfernalManor.Item;
+
+import java.util.*;
+import TheInfernalManor.GUI.*;
+import TheInfernalManor.Actor.*;
+import TheInfernalManor.Engine.*;
+
+public class EquipmentAffexFactory implements GUIConstants
+{
+
+}
